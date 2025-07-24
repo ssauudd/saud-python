9ede1383dd40059a2f5925b733496df6c6472ba2
SAUD PATEL <Saudpatel021@gmail.com>
2025-07-24 17:25:18 +0530

    Add files via upload

---
+++ 1.5.py
@@ -0,0 +1,19 @@
+marks=float(input("enter your marks:"))
+
+if marks>=101:
+    print("invalid input")
+
+elif marks>=90:
+    print("grade A")
+
+elif marks>=75:
+    print("grade B")
+
+elif marks>=50:
+    print("grade C")
+
+elif marks>=35:
+    print("grade D")
+
+else:
+    print("fail")
---
+++ prac1.py
@@ -0,0 +1,13 @@
+a=5
+b=10
+c=15
+print(a+b+c)
+print(a-b-c)
+print(a*b*c)
+print(a/b/c)
+
+
+
+
+
+
